6f5f1184fac2b1bf00d2ddb739488ff8f43cb3b1
Kirill Gordienko <91422323+k1dobu3@users.noreply.github.com>
2023-10-27 14:43:07 +0300

    Create scrapper.py
    version 0.1

---
+++ scrapper.py
@@ -0,0 +1,57 @@
+import sqlite3
+import json
+import requests
+from bs4 import BeautifulSoup
+import datetime
+
+Front = {'VueJS', 'JS', 'JavaScript', 'HTML', 'CSS', 'ReactJS', 'Angular', 'ReactNative', 'TypeScript', 'Node.JS'}
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.124 YaBrowser/22.9.4.864 Yowser/2.5 Safari/537.36"
+}
+url = 'https://habr.com/ru/flows/develop/'
+response = requests.get(url=url, headers=headers)
+soup = BeautifulSoup(response.text, 'lxml')
+quotes = soup.find_all('article', class_='tm-articles-list__item')
+news_dict = {}
+
+def scraper():
+  for n, i in enumerate(quotes, start=1):
+      # try/except исключает возможность пустого пользователям в статьях от самого портала Habr
+      try:
+          itemUser = i.find('a', class_='tm-user-info__username').text.strip()
+      except:
+          itemUser = 'Habr'
+      itemName = i.find('h2').text
+      itemTagsNoneFormat = i.find('div', class_='tm-article-snippet__hubs')  # tags
+      if itemTagsNoneFormat is not None:
+        itemTagsProm = itemTagsNoneFormat.replace('#', '')
+      else:
+        itemTagsProm = 'Отсутсвует'
+      itemTags = itemTagsProm.replace('*', '')
+      itemDescr = i.find('div', class_='article-formatted-body').text.strip()
+      itemUrl = f"https://habr.com{i.find('a', class_='tm-title__link', href=True).get('href')}"
+      itemUserUrl = f"https://habr.com{i.find('a', href=True).get('href')}"
+      itemId = itemUrl.split('/')[-2]
+  
+      news_dict[itemId] = {
+          "itemName": itemName,
+          "itemUser": itemUser,
+          "itemTags": itemTags,
+          "itemDescr": itemDescr,
+          "itemUrl": itemUrl,
+          "itemUserUrl": itemUserUrl
+      }
+      print(news_dict[itemId])
+  
+  with open("news_dict.json", "w") as file:
+      json.dump(news_dict, file, indent=4, ensure_ascii=False)
+  print("Парсер отработал")
+
+
+def main():
+  scrapper()
+
+
+if __name__ == '__main__':
+    main()
